2bf100e9e10350d9c90cf41e0e7a2075954f85f9
naXeer rehman <naxeerrehman@gmail.com>
2024-12-26 08:30:26 +0000

    Update OrdersDetail.js

--- frontend/src/components/OrdersDetail.js
+++ frontend/src/components/OrdersDetail.js
@@ -193,7 +193,7 @@ const OrdersDetail = () => {
           onClick={() => downloadPDF(order)} // Call the downloadPDF function when clicked
           className="mt-4 px-6 py-2 bg-blue-500 text-white font-bold rounded"
         >
-          Download PDF
+          Download The Invoice
         </button>
       </div>
     </div>
