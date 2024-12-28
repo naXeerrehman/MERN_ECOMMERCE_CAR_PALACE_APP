9736d3d2827d4f754006f7e92ba6f14bd303b08d
naXeer rehman <naxeerrehman@gmail.com>
2024-12-28 08:11:14 +0000

    Update UploadVehicle.js

--- frontend/src/components/UploadVehicle.js
+++ frontend/src/components/UploadVehicle.js
@@ -171,7 +171,7 @@ const UploadVehicle = () => {
                     <button
                       type="button"
                       onClick={() => handleRemoveImage(index)}
-                      className="absolute top-0 right-0 bg-red-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center"
+                      className="absolute top-0 right-0 bg-red-500 text-white text-xs rounded-full w-5 h-[20px] flex items-center justify-center"
                     >
                       x
                     </button>
