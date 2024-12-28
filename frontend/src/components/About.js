c2fa5559bf441ab2ec3df7dfb28bb4452a90c98f
naXeer rehman <naxeerrehman@gmail.com>
2024-12-28 06:44:59 +0000

    Update About.js

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,201 +1,227 @@
-import React, { useState } from "react";
-import axios from "axios";
-import TextEditor from "./TextEditor";
+import React, { useState, useEffect } from "react";
+import { FaRegEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+import { faCar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { jwtDecode } from "jwt-decode";
 
-const UploadVehicle = () => {
-  const [type, setType] = useState("");
-  const [brand, setBrand] = useState("");
-  const [model, setModel] = useState("");
-  const [price, setPrice] = useState("");
-  const [stock, setStock] = useState("");
-  const [description, setDescription] = useState("");
-  const [images, setImages] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [message, setMessage] = useState("");
+const AboutPage = () => {
+  const [icons, setIcons] = useState([]);
+  const [editingIconId, setEditingIconId] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [editingDetails, setEditingDetails] = useState({
+    link: "",
+    image: null,
+  });
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
 
-  const handleFileChange = (e) => {
-    setImages([...e.target.files]);
-  };
 
-  const handleRemoveImage = (index) => {
-    const newImages = images.filter((_, i) => i !== index);
-    setImages(newImages);
-  };
+  useEffect(() => {
+    fetchIcons();
+  }, []);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const formData = new FormData();
-
-    formData.append("type", type);
-    formData.append("model", model);
-    formData.append("brand", brand);
-    formData.append("price", price);
-    formData.append("stock", stock);
-    formData.append("description", description);
-    if (images) {
-      images.forEach((image) => {
-        formData.append("images", image);
-      });
-    }
-
+  const fetchIcons = async () => {
+    setIsLoading(true);
     try {
-      setLoading(true);
-      const response = await axios.post(
-        "http://localhost:5000/api/vehicles/create-vehicle",
-        formData,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
-      console.log("Vehicle created:", response.data);
-      setMessage("Vehicle created successfully!");
-      setLoading(false);
+      const response = await fetch("http://localhost:5000/api/icons");
+      const data = await response.json();
+      setIcons(data);
     } catch (error) {
-      console.error(
-        "Error creating vehicle:",
-        error.response?.data || error.message
+      console.error("Failed to fetch icons", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleEditIcon = async (id) => {
+    try {
+      const formData = new FormData();
+      formData.append("link", editingDetails.link);
+      formData.append("image", editingDetails.image);
+
+      const response = await fetch(`http://localhost:5000/api/icons/${id}`, {
+        method: "PUT",
+        body: formData,
+      });
+      const updatedIcon = await response.json();
+      setIcons((prevIcons) =>
+        prevIcons.map((icon) =>
+          icon._id === updatedIcon._id ? updatedIcon : icon
+        )
       );
-      setMessage("Error creating vehicle. Please try again.");
-      setLoading(false);
+      setEditingIconId(null);
+    } catch (error) {
+      console.error("Failed to edit icon", error);
+    }
+  };
+
+  const handleDeleteIcon = async (id) => {
+    try {
+      await fetch(`http://localhost:5000/api/icons/${id}`, {
+        method: "DELETE",
+      });
+      setIcons((prevIcons) => prevIcons.filter((icon) => icon._id !== id));
+    } catch (error) {
+      console.error("Failed to delete icon", error);
     }
   };
 
   return (
-    <div className="container mx-auto p-4">
-      <form
-        className="flex flex-col items-center w-full max-w-md mx-auto"
-        onSubmit={handleSubmit}
-      >
-        <h1 className="bg-black text-white w-full text-2xl rounded-sm p-2 font-semibold text-center">
-          Upload Vehicle
+    <div className="mb-10">
+      {/* Header Section */}
+      <header className="py-12 text-center">
+        <h1 className="text-4xl font-bold text-white mb-4 bg-black w-[300px] rounded-md mx-auto">
+          About Us
         </h1>
-        <div className="flex flex-col gap-y-4 w-full">
-          <div>
-            <label>Type</label>
-            <input
-              type="text"
-              placeholder="Enter Vehicle Type (Car, Motorcycle, Cycle)"
-              value={type}
-              className="border border-gray-700 px-2 rounded-sm w-full py-1"
-              required
-              onChange={(e) => setType(e.target.value)}
-            />
-          </div>
-          <div>
-            <label>Model</label>
-            <input
-              type="text"
-              placeholder="Enter Vehicle Model"
-              value={model}
-              className="border border-gray-700 px-2 rounded-sm w-full py-1"
-              required
-              onChange={(e) => setModel(e.target.value)}
-            />
-          </div>
-          <div>
-            <label>Brand</label>
-            <input
-              type="text"
-              placeholder="Enter Vehicle Brand"
-              value={brand}
-              className="border border-gray-700 px-2 rounded-sm w-full py-1"
-              required
-              onChange={(e) => setBrand(e.target.value)}
-            />
-          </div>
-          <div>
-            <label>Price</label>
-            <input
-              type="number"
-              placeholder="Enter Vehicle Price"
-              value={price}
-              className="border border-gray-700 px-2 rounded-sm w-full py-1"
-              required
-              onChange={(e) => setPrice(e.target.value)}
-            />
-          </div>
-          <div>
-            <label>Stock</label>
-            <input
-              type="number"
-              placeholder="Enter Stock Quantity"
-              value={stock}
-              className="border border-gray-700 px-2 rounded-sm w-full py-1"
-              required
-              onChange={(e) => setStock(e.target.value)}
-            />
-          </div>
+        <p className="text-lg">
+          Your one-stop destination for managing vehicles seamlessly and
+          efficiently.
+        </p>
+      </header>
 
-          <div>
-            <label>Description</label>
-            <TextEditor
-              value={description}
-              onChange={setDescription}
-              placeholder="Enter a detailed description for the vehicle"
-            />
-          </div>
+      {/* Main Content */}
+      <div className="max-w-6xl mx-auto px-6 grid grid-cols-1 md:grid-cols-2 gap-12">
+        {/* Who We Are Section */}
+        <section className="w-full bg-yellow-500 p-2 rounded-md">
+          <h2 className="text-3xl font-bold mb-4">Who We Are</h2>
+          <p className="text-lg mb-4">
+            We are a dedicated team providing comprehensive solutions for
+            vehicle management, ensuring a seamless experience for our clients.
+          </p>
+          <p>
+            Our platform offers tools and services tailored to meet the needs of
+            individuals and businesses alike.
+          </p>
+        </section>
 
-          <div>
-            <label>Images</label>
-            <input
-              type="file"
-              multiple
-              required
-              className="border border-gray-700 px-2 rounded-sm w-full py-1"
-              onChange={handleFileChange}
-            />
-          </div>
+        {/* Our Mission Section */}
+        <section className="w-full bg-yellow-500 p-2 rounded-md">
+          <h2 className="text-3xl font-bold mb-4">Our Mission</h2>
+          <p className="text-lg mb-4">
+            At the core of our mission is a commitment to innovation,
+            sustainability, and excellence. We aim to revolutionize the vehicle
+            management industry by providing tools that empower individuals and
+            businesses to make informed decisions and optimize their resources.
+          </p>
+        </section>
 
-          {images.length > 0 && (
-            <div className="mt-4">
-              <h2 className="text-xl">Selected Images:</h2>
-              <div className="flex flex-wrap gap-4 mt-2">
-                {images.map((image, index) => (
-                  <div key={index} className="relative">
-                    <img
-                      src={URL.createObjectURL(image)}
-                      alt={`image-${index}`}
-                      className="w-[160px] h-20 object-cover rounded-md"
-                    />
-                    <button
-                      type="button"
-                      onClick={() => handleRemoveImage(index)}
-                      className="absolute top-0 right-0 bg-red-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center"
-                    >
-                      x
-                    </button>
+        {/* Our Services Section */}
+        <section className="w-full bg-yellow-500 p-2 rounded-md">
+          <h2 className="text-3xl font-bold mb-4">Our Services</h2>
+          <ul className="list-disc pl-6 text-lg">
+            <li>Vehicle tracking and management</li>
+            <li>Real-time updates and notifications</li>
+            <li>Maintenance scheduling and reminders</li>
+            <li>Customer support for all inquiries</li>
+          </ul>
+        </section>
+
+        {/* Icons Section */}
+        <section className="w-full bg-yellow-500 p-2 rounded-md">
+          <h2 className="text-3xl font-bold">Contact Us</h2>
+          <h2 className="text-lg">You can contact us by</h2>
+          {isLoading ? (
+            <div className="flex flex-col justify-center items-center mt-5">
+              <FontAwesomeIcon
+                icon={faCar}
+                size="3x"
+                className="animate-zoomInOut" // Apply the zoom-in/zoom-out animation
+              />
+              <div className="font-bold ml-2 mt-2">Loading...</div>
+            </div>
+          ) : (
+            <div className="grid grid-cols-8 w-[500px]">
+              {icons.map((icon) => (
+                <div key={icon._id}>
+                  <div className="w-[100px]">
+                    {editingIconId === icon._id ? (
+                      <>
+                        <input
+                          type="text"
+                          value={editingDetails.link}
+                          onChange={(e) =>
+                            setEditingDetails({
+                              ...editingDetails,
+                              link: e.target.value,
+                            })
+                          }
+                          placeholder="Enter link"
+                          className="border p-2 w-[300px] relative bottom-[100px]"
+                        />
+                        <input
+                          type="file"
+                          onChange={(e) =>
+                            setEditingDetails({
+                              ...editingDetails,
+                              image: e.target.files[0],
+                            })
+                          }
+                          className="border p-2 w-[300px] relative bottom-[100px]"
+                        />
+                      </>
+                    ) : (
+                      <a
+                        href={icon.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <img
+                          src={icon.image}
+                          alt={icon.link}
+                          className="w-[50px] h-[50px] rounded-full border"
+                        />
+                      </a>
+                    )}
                   </div>
-                ))}
-              </div>
+                  
+                  {/* Buttons */}
+                  {/* Edit button*/}
+                  <div className="py-1 w-[100px] flex">
+                    {editingIconId === icon._id ? (
+                      <>
+                        <button
+                          onClick={() => handleEditIcon(icon._id)}
+                          className="bg-green-500 text-white px-2 py-1 rounded-md mr-2"
+                        >
+                          Save
+                        </button>
+                        <button
+                          onClick={() => setEditingIconId(null)}
+                          className="bg-gray-500 text-white px-4 py-2 rounded-md block w-full"
+                        >
+                          Cancel
+                        </button>
+                      </>
+                    ) : (
+                      <>
+                        <button
+                          onClick={() => {
+                            setEditingIconId(icon._id);
+                            setEditingDetails({ link: icon.link, image: null });
+                          }}
+                          className="bg-yellow-500 border text-white px-1 py-1 rounded-md mr-2"
+                        >
+                          <FaRegEdit />
+                        </button>
+                        {/* Delete button */}
+                        <button
+                          onClick={() => handleDeleteIcon(icon._id)}
+                          className="bg-red-500 text-white px-1 py-1 rounded-md"
+                        >
+                          <MdDelete />
+                        </button>
+                      </>
+                    )}
+                  </div>
+                </div>
+              ))}
             </div>
           )}
-          {message && (
-            <div
-              className={`mb-2 mt-2 border border-black px-2 rounded-md text-lg ${
-                message.includes("successfully")
-                  ? "text-green-500"
-                  : "text-red-500"
-              }`}
-            >
-              {message}
-            </div>
-          )}
-          <button
-            type="submit"
-            disabled={loading}
-            className={`bg-black text-white w-full rounded-sm text-lg p-2 font-semibold ${
-              loading ? "opacity-50 cursor-not-allowed" : ""
-            }`}
-          >
-            {loading ? "Uploading..." : "Upload Vehicle"}
-          </button>
-        </div>
-      </form>
+        </section>
+      </div>
     </div>
   );
 };
 
-export default UploadVehicle;
+export default AboutPage;
