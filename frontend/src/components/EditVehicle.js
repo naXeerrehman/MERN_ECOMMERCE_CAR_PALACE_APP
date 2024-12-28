e3ab3e199dd94b6030153b9c99abd0130080c8c2
naXeer rehman <naxeerrehman@gmail.com>
2024-12-28 08:16:58 +0000

    Update EditVehicle.js

--- frontend/src/components/EditVehicle.js
+++ frontend/src/components/EditVehicle.js
@@ -275,7 +275,7 @@ const EditVehicle = () => {
                     <button
                       type="button"
                       onClick={() => handleRemoveNewImage(index)}
-                      className="absolute top-0 right-0 bg-red-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center"
+                      className="absolute top-0 right-0 bg-red-500 text-white text-xs rounded-full w-5 h-[18px] flex items-center justify-center"
                     >
                       x
                     </button>
